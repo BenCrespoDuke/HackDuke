2c5e95defb4b211a8fbf1ff49c16426420eb0a97
nyu12345 <nathan.yu@duke.edu>
2020-12-05 10:53:09 -0500

    moved calculation to ui

--- app/src/main/java/com/example/hackduke/ui/calculation/Calculation.java
+++ app/src/main/java/com/example/hackduke/ui/calculation/Calculation.java
@@ -1,11 +1,13 @@
-package com.example.hackduke;
+package com.example.hackduke.ui.calculation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.*;
 
 import android.os.Bundle;
 
-public class Calculation extends AppCompatActivity {
+import com.example.hackduke.R;
 
+public class Calculation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
