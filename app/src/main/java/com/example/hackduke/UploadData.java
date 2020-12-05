c4be110c74f049f7d3fc1a069352c433f657064e
Benjamin Crespo <bjcrespo123@gmail.com>
2020-12-05 15:56:02 -0500

    Able to upload meal data to cloud

---
+++ app/src/main/java/com/example/hackduke/UploadData.java
@@ -0,0 +1,49 @@
+package com.example.hackduke;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UploadData {
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public UploadData(){
+
+    }
+
+    public void uploadMeal(String Uid, String date, double carbon, String[] foodStuff){
+        Map<String,Object> meal = new HashMap<>();
+        meal.put("Uid",Uid);
+        meal.put("date",date);
+        meal.put("carbonAmount",carbon);
+        meal.put("Food Stuff", foodStuff);
+        db.collection("meals").add(meal).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+
+                Log.d("Cloud Activity","The meal upload with ID "+documentReference.getId());
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Log.w("Cloud Activity", "ERROR adding document",e);
+                    }
+                });
+
+    }
+
+
+}
+
+
