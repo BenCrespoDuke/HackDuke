e90c5c1854ce40976603c4e97798cdb7a956735d
Louis Hu <louis.hu@duke.edu>
2020-12-05 11:20:18 -0500

    results

--- app/src/main/java/com/example/hackduke/ui/results/ResultsFragment.java
+++ app/src/main/java/com/example/hackduke/ui/results/ResultsFragment.java
@@ -1,4 +1,4 @@
-package com.example.hackduke.ui.calculation;
+package com.example.hackduke.ui.results;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,19 +13,18 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hackduke.R;
-import com.example.hackduke.ui.dashboard.DashboardViewModel;
 
-public class CalculationFragment extends Fragment {
+public class ResultsFragment extends Fragment {
 
-    private CalculationViewModel calculationViewModel;
+    private ResultsViewModel resultsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        calculationViewModel =
-                new ViewModelProvider(this).get(CalculationViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        calculationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        resultsViewModel =
+                new ViewModelProvider(this).get(ResultsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        resultsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
