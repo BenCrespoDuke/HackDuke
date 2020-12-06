4524a0c396e524a867dfe6169254224dfb4d9adb
Benjamin Crespo <bjcrespo123@gmail.com>
2020-12-06 13:45:13 -0500

    Gets Friend Requests

--- app/src/main/java/com/example/hackduke/AddFriendsActivity.java
+++ app/src/main/java/com/example/hackduke/AddFriendsActivity.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddFriendsActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
+public class AddFriendsActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +28,34 @@ public class AddFriendsActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.Name);
         name.setText(friendName);
+
+        Query query = db.collection("user").whereEqualTo("Uid",0);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful() == true) {
+                    ArrayList<String> friendreq = new ArrayList<String>();
+
+                    for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                        //DocID = documentSnapshot.getId();
+                        friendreq = (ArrayList<String>) documentSnapshot.get("friend requests");
+                    }
+                    Query query1 = db.collection("users").whereArrayContains("email",friendreq);
+                    query1.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            ArrayList<Friend> friendsRequesting = new  ArrayList<Friend>();
+                            if(task.isSuccessful()==true){
+                                for (DocumentSnapshot documentSnapshot:task.getResult()) {
+                                    friendsRequesting.add(new Friend(documentSnapshot.getData()));
+                                }
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
     }
 }
