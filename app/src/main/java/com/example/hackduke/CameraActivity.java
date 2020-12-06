fe77e2b8ef91fbd95293616b300fae5ca9bdb42e
Justin Jang <justin.jang@duke.edu>
2020-12-05 16:22:35 -0800

    Camera button work

--- app/src/main/java/com/example/hackduke/CameraActivity.java
+++ app/src/main/java/com/example/hackduke/CameraActivity.java
@@ -28,17 +28,6 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Assign Variable
         imageView = findViewById(R.id.image_view);
