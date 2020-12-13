3678065c2ea6ce3cdc8f3d146dab9aeb6a26214f
nyu12345 <nathan.yu@duke.edu>
2020-12-13 18:51:45 -0500

    Finished number only input

--- app/src/main/java/com/example/hackduke/ResultActivity.java
+++ app/src/main/java/com/example/hackduke/ResultActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -21,77 +22,83 @@ import java.util.Arrays;
 public class ResultActivity extends AppCompatActivity {
     private Button cam_button;
     private double size = 1.0;
-    private EditText input;
-    private AlertDialog.Builder alert;
+    EditText input;
+    boolean digitsOnly;
+    boolean isValidInput = false;
+    AlertDialog.Builder alert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        boolean isValidInput = false;
-        while(isValidInput == false) {
-            alert = new AlertDialog.Builder(this);
-            alert.setMessage("Estimated Amount of Food(lbs)");
+        alert = new AlertDialog.Builder(this);
+        alert.setMessage("Estimated Amount of Food(lbs)");
 
-            input = new EditText(this);
-            String text = input.getText().toString();
-            boolean digitsOnly = TextUtils.isDigitsOnly(text);
-            if(digitsOnly == true) {
-                isValidInput = true;
-            }
-            else {
-                alert.setMessage("NOOOO");
-            }
-        }
-
+        input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER |  InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        digitsOnly = TextUtils.isDigitsOnly(input.getText().toString());
         alert.setView(input);
 
         alert.setPositiveButton("Ok", (dialog, whichButton) -> {
-            ConstraintLayout w = findViewById(R.id.whole);
-            w.setVisibility(View.VISIBLE);
-            size = Double.parseDouble(input.getText().toString());
-            Bundle extras = getIntent().getExtras();
-            ArrayList<String> predictList = extras.getStringArrayList("data");
-            Intent intent = getIntent();
-            Bitmap bitmap = (Bitmap) intent.getParcelableExtra("bit");
-            String name = searchFoodList(predictList);
-            Calculation calc = new Calculation(name,size);
-            calc.calculate();
-
-            TextView foodName = findViewById(R.id.foodName);
-            foodName.setText(name);
-
-            TextView foodGroup= findViewById(R.id.foodGroup);
-            foodGroup.setText(calc.getGroup().getName());
-
-            TextView register = findViewById(R.id.registered);
-
-            TextView score = findViewById(R.id.score);
-
-            TextView recommendation = findViewById(R.id.Recommendation);
-            if(calc.currentGroup.getName().equals("Other")) {
-                score.setText("N/A");
-                register.setText("Failed to Register Food");
-                recommendation.setText("Sorry, your documented food is not found");
-            }
-            else {
-                score.setText(""+calc.getCo2() +" lbs");
-                register.setText("Successfully Registered Food");
-                recommendation.setText(calc.getRec());
-                UploadData helper = new UploadData();
-                        helper.uploadMeal(0+"","",calc.getCo2(),calc.getName(),true);
-            }
-
-            ImageView img = findViewById(R.id.imageView2);
-            img.setImageBitmap(bitmap);
-
-            cam_button = findViewById(R.id.backToCam);
-            cam_button.setBackgroundColor(Color.parseColor("#3ead60"));
-            cam_button.setOnClickListener(v -> openHome());
-
+            display(input);
         });
 
         alert.show();
 
+    }
+
+    /*
+    private boolean validateFields() {
+        if (digitsOnly == false) {
+            input.setError("Your Input is Invalid");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    **/
+
+    public void display(EditText in) {
+        ConstraintLayout w = findViewById(R.id.whole);
+        w.setVisibility(View.VISIBLE);
+        size = Double.parseDouble(in.getText().toString());
+        Bundle extras = getIntent().getExtras();
+        ArrayList<String> predictList = extras.getStringArrayList("data");
+        Intent intent = getIntent();
+        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("bit");
+        String name = searchFoodList(predictList);
+        Calculation calc = new Calculation(name,size);
+        calc.calculate();
+
+        TextView foodName = findViewById(R.id.foodName);
+        foodName.setText(name);
+
+        TextView foodGroup= findViewById(R.id.foodGroup);
+        foodGroup.setText(calc.getGroup().getName());
+
+        TextView register = findViewById(R.id.registered);
+
+        TextView score = findViewById(R.id.score);
+
+        TextView recommendation = findViewById(R.id.Recommendation);
+        if(calc.currentGroup.getName().equals("Other")) {
+            score.setText("N/A");
+            register.setText("Failed to Register Food");
+            recommendation.setText("Sorry, your documented food is not found");
+        }
+        else {
+            score.setText(""+calc.getCo2() +" lbs");
+            register.setText("Successfully Registered Food");
+            recommendation.setText(calc.getRec());
+            UploadData helper = new UploadData();
+            helper.uploadMeal(0+"","",calc.getCo2(),calc.getName(),true);
+        }
+
+        ImageView img = findViewById(R.id.imageView2);
+        img.setImageBitmap(bitmap);
+
+        cam_button = findViewById(R.id.backToCam);
+        cam_button.setBackgroundColor(Color.parseColor("#3ead60"));
+        cam_button.setOnClickListener(v -> openHome());
     }
 
     public String searchFoodList(ArrayList<String> list) {
